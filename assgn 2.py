aa8bfd11e4e1cab096c49631f72bc443421bda5b
Austine <isaiahomondi75@gmail.com>
2023-10-18 12:07:32 +0300

    first commit

---
+++ assgn 2.py
@@ -0,0 +1,28 @@
+maths = float(input("Enter math marks: "))
+physics = float(input("Enter physics marks: "))
+geo = float(input("Enter geography marks: "))
+chem = float(input("Enter chemistry marks: "))
+
+
+def calculate_grade(maths, physics, geo, chem):
+    # Check for negative values or values greater than 100
+    for mark in [maths, physics, geo, chem]:
+         if mark < 0 or mark > 100:
+            return "Unrecognized marks/avg"
+# Calculate the average
+    avg = (maths + physics + geo + chem) / 4
+
+# Determine the grade based on the average
+    if 0 <= avg <= 30:
+        return 'D'
+    elif 31 <= avg <= 50:
+        return 'C'
+    elif 51 <= avg <= 70:
+        return 'B'
+    elif 71 <= avg <= 100:
+        return 'A'
+
+
+# Calculate and print the grade
+result = calculate_grade(maths, physics, geo, chem)
+print("Grade:", result)
---
+++ operators.py
@@ -0,0 +1,7 @@
+ladies = 50
+men = 70
+
+
+
+ Equal = (ladies/men)
+print(Equal)
